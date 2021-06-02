029817099bf8db9ed568f65866ca6e717651fb92
isadays <isah.pld@gmail.com>
2021-06-02 08:48:51 -0300

    Atualizacao Models

--- App/Model/TipoTransacao.php
+++ App/Model/TipoTransacao.php
@@ -4,8 +4,22 @@ class TipoTransacao{
     private $tipo;
 
 }
-
-
+public function setIDTransacao($idtransacao)
+{
+    $this->idtransacao = $idtransacao;
+}
+public function getIDTransacao()
+{
+    return $this-> idtransacao;
+}
+public function setTipo($tipo)
+{
+    $this->tipo = $tipo;
+}
+public function getTipo()
+{
+    return $this-> tipo;
+}
 
 
 
