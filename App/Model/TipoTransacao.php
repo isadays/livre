33b12882310235d14a8f98aca699feadb61ff150
isadays <isah.pld@gmail.com>
2021-06-05 11:21:18 -0300

    Finalizando Models Transacoes

--- App/Model/TipoTransacao.php
+++ App/Model/TipoTransacao.php
@@ -20,6 +20,57 @@ public function getTipo()
     return $this-> tipo;
 }
 
+// Metodo carregar Tipo Transacoes.
+public function carregarTransacoes($tipo)
+{
+    require_once './ConexaoBD.php';
+
+    $con = new ConexaoBD();
+    $conn = $con->conectar();
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $sql = "SELECT * FROM tipo_transacao WHERE id = " . $idtransacao;
+    $re = $conn->query($sql);
+    $r = $re->fetch_object();
+    if ($r != null) {
+        $this->idtransacao = $r->idtransacao;
+        $this->tipo = $r->tipo;
+
+        $conn->close();
+        return true;
+    } else {
+        $conn->close();
+        return false;
+    }
+}
+
+// Metodo atualizar banco de dados.
+public function atualizarBD()
+{
+    require_once './ConexaoBD.php';
+
+    $con = new ConexaoBD();
+    $conn = $con->conectar();
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $sql = "UPDATE tipo_transacoes SET 
+    id = '" . $this->idtransacao . "',
+    tipo = '" . $this->tipo . "',
+    WHERE id = '" . $this->idtransacao . "'";
+
+    if ($conn->query($sql) === TRUE) {
+        $conn->close();
+        return TRUE;
+    } else {
+        $conn->close();
+        return FALSE;
+    }
+}
+
 }
 
 ?>
--- App/Model/Transacao.php
+++ App/Model/Transacao.php
@@ -50,5 +50,95 @@ public function getIDLivro()
 {
     return $this-> id_livro;
 }
+
+
+public function inserirBD()
+{
+    require_once './ConexaoBD.php';
+
+    $con = newConexaoBD();
+    $conn = $con->conectar();
+    if ($conn->connect_error) {
+        die("Connection failed: $conn->connect_error");
+    }
+
+    $sql = "INSERT INTO transacoes (id_transacao, valor, data,tipo, id_usuario,id_livro) VALUES
+        (
+         '" . $this->idtransacao . "',
+         '" . $this->valor . "',
+         '" . $this->data . "',
+         '" . $this->tipo . "',
+         '" . $this->id_usuario . "',
+         '" . $this->id_livro . "',
+        )";
+        
+    if ($conn->query($sql) == TRUE) {
+        $this->id = mysqli_insert_id($conn);
+        $conn->close();
+        return TRUE;
+    }else {
+        $conn->close();
+        return FALSE;
+    }
+}
+
+// Metodo carregar Transacoes.
+public function carregarTransacoes($idtransacao)
+{
+    require_once './ConexaoBD.php';
+
+    $con = new ConexaoBD();
+    $conn = $con->conectar();
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $sql = "SELECT * FROM transacoes WHERE idtransacao = " . $idtransacao;
+    $re = $conn->query($sql);
+    $r = $re->fetch_object();
+    if ($r != null) {
+        $this->idtransacao = $r->idtransacao;
+        $this->valor = $r->valor;
+        $this->data = $r->data;
+        $this->tipo = $r->tipo;
+        $this->id_usuario = $r->id_usuario;
+        $this->id_livro = $r->id_livro;
+
+        $conn->close();
+        return true;
+    } else {
+        $conn->close();
+        return false;
+    }
+}
+
+// Metodo atualizar banco de dados.
+public function atualizarBD()
+{
+    require_once './ConexaoBD.php';
+
+    $con = new ConexaoBD();
+    $conn = $con->conectar();
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $sql = "UPDATE transacoes SET 
+    id_transacao = '" . $this->id_transacao . "',
+    valor = '" . $this->valor. "',
+    data = '" . $this->data . "',
+    tipo = '" . $this->tipo . "',
+    id_usuario = '" . $this->id_usuario . "',
+    id_livro = '" . $this->id_livro . "',
+    WHERE id_usuario = '" . $this->id_usuario . "'";
+
+    if ($conn->query($sql) === TRUE) {
+        $conn->close();
+        return TRUE;
+    } else {
+        $conn->close();
+        return FALSE;
+    }
+}
 }
 ?>
