f71e0af696fbf6abb52f74ad795636204bb6ffa7
Patrik Rufino <patrik.rufino@gmail.com>
2021-06-07 23:53:17 -0300

    Criando a tela de exibição dos livros

---
+++ App/View/listalivros.php
@@ -0,0 +1,45 @@
+<?php
+require_once 'includes/header.php';
+?>
+<div class="container content" xmlns:a="http://www.w3.org/1999/html">
+    <div class="row">
+        <!--    Lista de filtros-->
+        <div class="col-3">
+            <ul>
+                <li>Drama</li>
+                <li>Drama</li>
+                <li>Drama</li>
+                <li>Drama</li>
+                <li>Drama</li>
+            </ul>
+        </div>
+
+        <!--    Área de cards dos livros-->
+        <div class="col-9">
+            <div class="card shadow-lg mt-5">
+                <div class="row">
+                    <div class="col-2 m-3">
+                        <img src="../../Public/Img/livre_logo.png" class="card-img-top " style="max-width: 80px">
+                    </div>
+
+                    <div class="col">
+                        <div class="card-title">
+                            <div class="col mt-3 align-content-center">
+                                <h2> A volta dos que não foram</h2>
+                            </div>
+                        </div>
+                        <div class="card-body">
+                            <p>apsdjkasfjkpajpfomcpmcpoajpojfooap fpasjp pnaipasnpc k paipan k if lak kl pic ao c o </p>
+                        </div>
+                    </div>
+                    <div class="card-footer">
+                        <a class="m-3" href="#">Chamar no WhatsApp</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <?php
+    require_once 'includes/footer.php';
+    ?>
